cd7393c7ca6b336f77fa141024161df64212f29a
6MA-606 <zyxma.6666@hotmail.com>
2024-01-27 12:18:33 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+ 
+<template>
+  <div class="h-svh flex justify-center items-center">
+    <div class="text-4xl font-bold">
+      Hello Point-of-Vue
+    </div>
+  </div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
