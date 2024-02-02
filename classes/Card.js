c5c0101ab8f7ed80aed11275ca4675b4f1fa33e1
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-02-02 18:55:09 +0700

    add: class-card

---
+++ classes/Card.js
@@ -0,0 +1,13 @@
+export class Card{
+    constructor(id,name,arts,color){
+        this.id = id
+        this.name = name
+        this.arts = arts
+        this.color = color
+        this.isFliped = false
+    }
+    setFlip(flipState){
+        this.isFliped = flipState
+    }
+
+}
